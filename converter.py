15d4107656afabbdcc9d2952daefe4b00b626f4e
Jonathan Kwak <159591276+JonathanKwak@users.noreply.github.com>
2025-07-01 12:05:27 -0400

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-units = [
+UNITS = [
     "Meters",
     "Centimeters",
     "Kilometers",
@@ -11,10 +11,10 @@ units = [
     "Miles"
 ]
 
-unit_to_meter = {
+UNIT_TO_METER = {
     "Meters" : 1.0, # 1 meter = 1 meter
     "Centimeters" : 0.01, # 1 cm = 0.01 meters
-    "Kilometers" : 1000, # 1 km = 1000 meters
+    "Kilometers" : 1000.0, # 1 km = 1000 meters
 
     "Yards" : 0.9144, # 1 yard = 0.9144 meters
     "Inches" : 0.0254, # 1 inch = 0.0254 meters
@@ -32,14 +32,22 @@ def convert():
         base = float(value)
 
         # convert 'from' to meters first
-        meters = base * unit_to_meter[unit_from]
-        result = meters / unit_to_meter[unit_to]
+        meters = base * UNIT_TO_METER[unit_from]
+        result = meters / UNIT_TO_METER[unit_to]
 
         result_var.set(f"{value} {unit_from.lower()} is about {result} {unit_to.lower()}.")
     except ValueError:
         result_var.set("Invalid input.")
 
-    
+def on_entry_change(*args):
+    value = current_value.get()
+
+    try:
+        base = float(value)
+
+        button.pack()
+    except ValueError:
+        button.pack_forget()
 
 root = tk.Tk()
 root.title("Unit converter")
@@ -49,6 +57,7 @@ root.resizable(False, False) # not resizable in x or y
 selected_unit = tk.StringVar(value="Meters") # variable class for this specific library
 unit_to_convert_to = tk.StringVar(value="Kilometers")
 current_value = tk.StringVar(value=10)
+current_value.trace_add("write", on_entry_change)
 
 style = ttk.Style()
 style.theme_use("clam")
@@ -59,13 +68,13 @@ entry.pack(pady=10)
 from_text = ttk.Label(root, text="From:")
 from_text.pack()
 
-convert_from = ttk.Combobox(root, values=units, state="readonly", textvariable=selected_unit)
+convert_from = ttk.Combobox(root, values=UNITS, state="readonly", textvariable=selected_unit)
 convert_from.pack(pady=2)
 
 to_text = ttk.Label(root, text="To:")
 to_text.pack()
 
-convert_to = ttk.Combobox(root, values=units, state="readonly", textvariable=unit_to_convert_to)
+convert_to = ttk.Combobox(root, values=UNITS, state="readonly", textvariable=unit_to_convert_to)
 convert_to.pack(pady=2)
 
 button = ttk.Button(root, text="Convert", command=convert, width=len("Convert"))
