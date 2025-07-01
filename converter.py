8c4a5c6f08735d0ca668695ed090c0befb1497e4
Jonathan Kwak <159591276+JonathanKwak@users.noreply.github.com>
2025-06-30 21:23:15 -0400

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+from tkinter import ttk
+
+units = [
+    "Meters",
+    "Centimeters",
+    "Kilometers",
+
+    "Yards",
+    "Inches",
+    "Miles"
+]
+
+unit_to_meter = {
+    "Meters" : 1.0, # 1 meter = 1 meter
+    "Centimeters" : 0.01, # 1 cm = 0.01 meters
+    "Kilometers" : 1000, # 1 km = 1000 meters
+
+    "Yards" : 0.9144, # 1 yard = 0.9144 meters
+    "Inches" : 0.0254, # 1 inch = 0.0254 meters
+    "Miles" : 1609.34 # 1 mile = 1609.34 meters
+}
+
+def convert():
+    unit_from = selected_unit.get()
+    unit_to = unit_to_convert_to.get()
+    value = current_value.get()
+
+    print(f"Convert {value} {unit_from} to {unit_to}.")
+
+    try:
+        base = float(value)
+
+        # convert 'from' to meters first
+        meters = base * unit_to_meter[unit_from]
+        result = meters / unit_to_meter[unit_to]
+
+        result_var.set(f"{value} {unit_from.lower()} is about {result} {unit_to.lower()}.")
+    except ValueError:
+        result_var.set("Invalid input.")
+
+    
+
+root = tk.Tk()
+root.title("Unit converter")
+root.geometry("300x300")
+root.resizable(False, False) # not resizable in x or y
+
+selected_unit = tk.StringVar(value="Meters") # variable class for this specific library
+unit_to_convert_to = tk.StringVar(value="Kilometers")
+current_value = tk.StringVar(value=10)
+
+style = ttk.Style()
+style.theme_use("clam")
+
+entry = ttk.Entry(root, textvariable=current_value)
+entry.pack(pady=10)
+
+from_text = ttk.Label(root, text="From:")
+from_text.pack()
+
+convert_from = ttk.Combobox(root, values=units, state="readonly", textvariable=selected_unit)
+convert_from.pack(pady=2)
+
+to_text = ttk.Label(root, text="To:")
+to_text.pack()
+
+convert_to = ttk.Combobox(root, values=units, state="readonly", textvariable=unit_to_convert_to)
+convert_to.pack(pady=2)
+
+button = ttk.Button(root, text="Convert", command=convert, width=len("Convert"))
+button.pack(pady=10)
+
+result_var = tk.StringVar() # no value by default
+
+result_label = tk.Label(root, textvariable=result_var)
+result_label.pack(pady=10)
+
+root.mainloop()
